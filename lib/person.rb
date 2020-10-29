0114527faef6e4906d2a6abaaacc14821185907d
Simran <mdrsimran@gmail.com>
2020-10-29 15:35:54 -0500

    commit tree 0ec2b130f7a777b5528cd0d72665ba13f0502254

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+
+    def initializw(name,job)
+        @name=name
+        @job=job
+    end
+
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name=name
+    end
+
+    def job
+        @job
+    end    
+    
+    def job=(job)
+        @job=job
+    end
+
+
+end
